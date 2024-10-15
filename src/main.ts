7fb5a5e37ac8bf6cdd276883f4ce2f2d9c89e653
Shardul Nalegave <shardul.nalegave.2005@gmail.com>
2024-10-16 00:15:29 +0530

    Perline Noise flow field - v1

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,85 @@
 
 import './style.css';
 import p5 from 'p5';
+import { Particle } from './particle';
 
 new p5((p: p5) => {
-  p.setup = () => setup(p);
-  p.draw = () => draw(p);
-  p.windowResized = () => windowResized(p);
+  let SCALE = 50;
+  let ZOFF = 0;
+  let NUM_PARTICLES = 500;
+  let PARTICLES: Particle[] = [];
+  let FLOW_FIELD: p5.Vector[][] = [];
+
+  p.windowResized = () => {
+    p.resizeCanvas(window.innerWidth, window.innerHeight, true);
+  };
+
+  p.setup = () => {
+    p.createCanvas(window.innerWidth, window.innerHeight);
+    p.background(0);
+
+    for (let i = 0; i < NUM_PARTICLES; i++) {
+      PARTICLES[i] = new Particle(p);
+    }
+
+    let COLS = p.width / SCALE;
+    let ROWS = p.height / SCALE;
+
+    let yoff = 0;
+    for (let y = 0; y < ROWS; y++) {
+      let row = [];
+      let xoff = 0;
+      for (let x = 0; x < COLS; x++) {
+        let angle = p.noise(xoff, yoff, ZOFF) * p.TWO_PI * 4;
+        let vec = p5.Vector.fromAngle(angle);
+        vec.setMag(0.5);
+        row.push(vec);
+        xoff += 0.1;
+      }
+      FLOW_FIELD.push(row);
+      yoff += 0.1;
+    }
+  };
+
+  p.draw = () => {
+    // p.background(0);
+    let COLS = p.width / SCALE;
+    let ROWS = p.height / SCALE;
+
+    p.stroke(255, 50);
+    p.strokeWeight(1);
+    p.noFill();
+
+    let yoff = 0;
+    for (let y = 0; y < ROWS; y++) {
+      let xoff = 0;
+      for (let x = 0; x < COLS; x++) {
+        let angle = p.noise(xoff, yoff, ZOFF) * p.TWO_PI * 4;
+        let vec = p5.Vector.fromAngle(angle);
+        vec.setMag(0.5);
+        FLOW_FIELD[y][x] = vec;
+        // p.push();
+        // p.translate(x*SCALE, y*SCALE);
+        // p.rotate(vec.heading());
+        // p.line(0, 0, SCALE, 0);
+        // p.pop();
+        xoff += 0.1;
+      }
+      yoff += 0.1;
+    }
+
+    ZOFF += 0.008;
+
+    for (let i = 0; i < NUM_PARTICLES; i++) {
+      let particle = PARTICLES[i];
+
+      let x = p.floor(particle.pos.x / SCALE);
+      let y = p.floor(particle.pos.y / SCALE);
+      particle.applyAccel(FLOW_FIELD[y][x]);
+
+      particle.update();
+      particle.edges(p);
+      particle.display(p);
+    }
+  };
 });
-
-function windowResized(p: p5) {
-  p.resizeCanvas(window.innerWidth, window.innerHeight, true);
-}
-
-function setup(p: p5) {
-  p.createCanvas(window.innerWidth, window.innerHeight, p.WEBGL);
-}
-
-function draw(p: p5) {
-  p.background(0);
-}
---
+++ src/particle.ts
@@ -0,0 +1,39 @@
+
+import p5 from 'p5';
+
+export class Particle {
+  pos: p5.Vector;
+  vel: p5.Vector;
+  accel: p5.Vector;
+  maxSpeed: number = 5;
+
+  constructor(p: p5) {
+    this.pos = p.createVector(p.random(p.width), p.random(p.height));
+    this.vel = p.createVector(0, 0);
+    this.accel = p.createVector(0, 0);
+  }
+
+  update() {
+    this.vel.add(this.accel);
+    this.vel.limit(this.maxSpeed);
+    this.pos.add(this.vel);
+    this.accel.mult(0);
+  }
+
+  applyAccel(accel: p5.Vector) {
+    this.accel.add(accel);
+  }
+
+  edges(p: p5) {
+    if (this.pos.x > p.width) this.pos.x = 0;
+    if (this.pos.x < 0) this.pos.x = p.width;
+    if (this.pos.y > p.height) this.pos.y = 0;
+    if (this.pos.y < 0) this.pos.y = p.height;
+  }
+
+  display(p: p5) {
+    p.stroke(255, 50);
+    p.strokeWeight(1);
+    p.point(this.pos);
+  }
+}
